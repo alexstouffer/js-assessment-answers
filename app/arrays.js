cdc52010c28b6c9fe2a929408b2976165b835dbf
JackJets <bizkite@hotmail.com>
2016-01-01 21:06:34 -0800

    Completed tests up till concat function test.

--- app/arrays.js
+++ app/arrays.js
@@ -25,29 +25,40 @@ exports.arraysAnswers = {
     });
     return result;
   },
-
+  //Splice will mutate the original array, and it's exactly what the next test calls for. Without subtracting from our index before proceeding through our for loop, we'll skip over the next value in the array.
   removeWithoutCopy : function(arr, item) {
-
+    for(var i = 0; i < arr.length; i++){
+      if (arr[i] == item){
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+    return arr;
   },
 
   append : function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate : function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend : function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail : function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat : function(arr1, arr2) {
-
+    var result = arr1 + arr2;
+    return result;
   },
 
   insert : function(arr, item, index) {
