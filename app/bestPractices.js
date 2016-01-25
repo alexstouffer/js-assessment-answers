f1de9e7a97de7b418181c42a81fe0f5109a07dd6
JackJets <bizkite@hotmail.com>
2016-01-24 21:52:59 -0800

    completed best practices.

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -8,7 +8,7 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.bestPracticesAnswers = {
   globals : function() {
-    myObject = {
+    var myObject = {
       name : 'Jory'
     };
 
@@ -17,19 +17,20 @@ exports.bestPracticesAnswers = {
 
   functions : function(flag) {
     if (flag) {
-      function getValue() { return 'a'; }
+      var getValue = function() { return 'a'; }
     } else {
-      function getValue() { return 'b'; }
+      var getValue = function() { return 'b'; }
     }
 
     return getValue();
   },
 
   parseInt : function(num) {
-    return parseInt(num);
+    return parseInt(num, 10);
   },
 
   identity : function(val1, val2) {
-
+    if(val1 === val2) return true;
+    return false;
   }
 };
