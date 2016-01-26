63430acc7485178d632c9292ab1fd676b490d6a7
JackJets <bizkite@hotmail.com>
2016-01-26 15:53:53 -0800

    cleaned up reverse string test by chaining split, reverse and join methods.

--- app/strings.js
+++ app/strings.js
@@ -2,15 +2,19 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+      var count = 0;
+      strArr = str.split('');
+      strArr.filter(function(item){
+         if(item > amount){
+             item.pop();
+         } 
+      });
+      str.join('');
   },
   wordWrap: function(str, cols) {
 
   },
   reverseString: function(str) {
-      var splitted = str.split('');
-      var reversed = splitted.reverse();
-      var joined = reversed.join('');
-      return joined;
+      return str.split('').reverse().join('');
   }
 };
