a7eebe8f971809c0c176636aa0ef8f10d7984a72
JackJets <bizkite@hotmail.com>
2016-01-28 20:10:34 -0800

    used function.apply to complete first test in functions section.

--- app/functions.js
+++ app/functions.js
@@ -2,11 +2,11 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray : function(fn, arr) {
-
+      return fn.apply(this, arr);
   },
 
   speak : function(fn, obj) {
-
+      return fn(obj);
   },
 
   functionFunction : function(str) {
