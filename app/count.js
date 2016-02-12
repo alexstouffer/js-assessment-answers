fd34a0922bae137cfca3dc681f3d130245162840
JackJets <bizkite@hotmail.com>
2016-02-11 16:22:54 -0800

    Counter function needs inner function called count to push console.log start number, and if condition is met, set timeout var to setTimeout function.

--- app/count.js
+++ app/count.js
@@ -2,9 +2,20 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
-    for(var i = start; i <= end; i++){
-      window.setTimeout(100);
-      return i;
+    var timeout;
+    function count(){
+        console.log(start++);
+        
+        if(start <= end){
+            timeout = setTimeout(count, 100);
+        }
     }
+    count();
+    
+    return {
+        cancel: function(){
+            timeout && clearTimeout(timeout);
+        }
+    };
   }
 };
